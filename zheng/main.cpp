81cdcfd70a5709d2993cbcd35ff46b1c77af2a9b
Nikita Mescheryakov <deffrian@protonmail.com>
2022-05-01 23:40:11 +0300

    n f can de specified

--- zheng/main.cpp
+++ zheng/main.cpp
@@ -19,19 +19,19 @@ void read_processes_from_config(const std::string &acceptors_config, ProtocolTcp
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        std::cout << "usage: port id elem config" << std::endl;
+        std::cout << "usage: n f port id elem config" << std::endl;
         assert(false);
     }
-    ProtocolTcp<LatticeSet> protocol(std::stoi(argv[1]));
-    read_processes_from_config(argv[4], protocol, std::stoi(argv[2]));
-    ZhengLA<LatticeSet> la(3, 8, std::stoi(argv[2]), protocol);
-    std::cout << "Start server. port: " << std::stoi(argv[1]) << std::endl;
+    ProtocolTcp<LatticeSet> protocol(std::stoi(argv[3]));
+    read_processes_from_config(argv[6], protocol, std::stoi(argv[4]));
+    ZhengLA<LatticeSet> la(std::stoi(argv[2]), std::stoi(argv[1]), std::stoi(argv[5]), protocol);
+    std::cout << "Start server. port: " << std::stoi(argv[3]) << std::endl;
     protocol.start(&la);
     std::this_thread::sleep_for(std::chrono::seconds(20));
     std::cout << "Server started" << std::endl;
 
     LatticeSet s;
-    s.insert(std::stoi(argv[3]));
+    s.insert(std::stoi(argv[5]));
     auto y = la.start(s);
     std::cout << "Answer: " << std::endl;
     for (auto elem : y.set) {
