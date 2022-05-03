b358f3de51bc3d3cbf6db27b5a723498913d5a59
Nikita Mescheryakov <deffrian@protonmail.com>
2022-05-03 06:05:19 +0300

    coordinator settings

--- coordinator/main.cpp
+++ coordinator/main.cpp
@@ -2,8 +2,15 @@
 #include "la_coordinator.h"
 #include "general/lattice.h"
 
-int main() {
-    LACoordinator<LatticeSet> coordinator(4, 1, 8090);
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        std::cout << "usage n f port" << std::endl;
+        assert(false);
+    }
+    uint64_t n = std::stoi(argv[1]);
+    uint64_t f = std::stoi(argv[2]);
+    uint64_t port = std::stoi(argv[3]);
+    LACoordinator<LatticeSet> coordinator(n, f, port);
 
     std::cout << "Done" << std::endl;
 }
